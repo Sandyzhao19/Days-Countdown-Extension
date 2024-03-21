6c7bc915c603a272891bfb4210a3abe65d156437
sandy <sandy@gymaware.com>
2024-03-21 17:08:47 +1100

    Add data table and update the extension logo

--- src/js/popup.js
+++ src/js/popup.js
@@ -9,6 +9,14 @@ import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// VueDatePicker
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+// Vue3EasyDataTable
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
+
 const vuetify = createVuetify({
   components,
   directives,
@@ -22,5 +30,10 @@ const vuetify = createVuetify({
   },
 })
 
+const app = createApp(Popup);
 
-createApp(Popup).use(vuetify).mount("#app");
+app.component('VueDatePicker', VueDatePicker);
+app.component('EasyDataTable', Vue3EasyDataTable);
+app.use(vuetify);
+
+app.mount("#app");
--- src/js/popup.vue
+++ src/js/popup.vue
@@ -1,53 +1,46 @@
 <template>
-  <div class="container text-center position-relative mt-4 mb-4">
-    <h1 class="title">
-      Days Countdown
-      <v-btn
-          class="mx-3"
-          density="compact"
-          icon="mdi-plus"
-          @click="addDayMatter"
-      ></v-btn>
-    </h1>
+  <div class="container position-relative mt-4 mb-4">
+    <v-row>
+      <v-img src="./images/icon-16x16.png" alt="Days Countdown" />
+      <h2 class="title">
+        Days Countdown
+        <v-btn
+            class="mx-3"
+            density="compact"
+            icon="mdi-plus"
+            @click="addDayMatter"
+        ></v-btn>
+      </h2>
+    </v-row>
+
 
     <v-row v-if="showDateForm" class="mx-2 mt-4">
       <v-col cols="6">
-        <v-text-field v-model="eventName" label="Enter Name" variant="outlined"></v-text-field>
+        <v-text-field v-model="inputTitle" label="Task Title" variant="outlined"></v-text-field>
       </v-col>
       <v-col cols="6">
-        <v-text-field v-model="selectedDate" label="Select Date" variant="outlined" append-inner-icon="mdi-calendar"
-                      @click:append-inner="openCalendarPicker"></v-text-field>
-        <v-menu v-model="calendarPicker" :close-on-content-click="false" :nudge-y="true">
-          <v-date-picker v-model="selectedDate" no-title></v-date-picker>
-        </v-menu>
-
-         <v-menu
-          ref="menu1"
-          v-model="calendarPicker"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="selectedDate"
-              label="Date"
-              hint="MM/DD/YYYY format"
-              persistent-hint
-              prepend-icon="event"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="selectedDate" no-title></v-date-picker>
-        </v-menu>
+        <VueDatePicker v-model="selectedDate" placeholder="Task Due" text-input/>
       </v-col>
     </v-row>
 
+
+    <EasyDataTable :headers="taskHeaders" :items="sampleTaskItems" table-class-name="customize-table" hide-footer
+                   class="ml-4">
+      <template #header="header">
+      </template>
+      <template #item-title="item">
+        <div class="d-flex align-center">
+          {{ item.title }}<br/>
+          {{ item.dueDate }}
+        </div>
+      </template>
+
+      <template #item-daysLeft="item">
+        <div class="d-flex align-center">
+          <h4 class="text-h4">{{ getDaysLeft(item.dueDate) }}</h4>
+        </div>
+      </template>
+    </EasyDataTable>
   </div>
 
 
@@ -62,14 +55,43 @@
     </blockquote>
   </div> -->
 </template>
+
 <script>
 export default {
   data() {
     return {
       showDateForm: false,
-      eventName: "",
+      inputTitle: "",
       selectedDate: null,
-      calendarPicker: false,
+
+      taskHeaders: [
+        {
+          text: "Title",
+          align: "start",
+          sortable: false,
+          value: "title",
+        },
+        {text: "Days Left", value: "daysLeft"}
+      ],
+
+      sampleTaskItems: [
+        {
+          id: 1,
+          title: "Assignment Due",
+          dueDate: "03/24/2024",
+        },
+        {
+          id: 2,
+          title: "Photo Shoot",
+          dueDate: "03/24/2024",
+        },
+        {
+          id: 3,
+          title: "Ultra Festival",
+          dueDate: "04/13/2024",
+        },
+      ],
+
       //   state: {
       //     quote: "",
       //     author: "",
@@ -81,36 +103,60 @@ export default {
       this.showDateForm = true;
     },
 
-    openCalendarPicker() {
-      this.calendarPicker = true;
-    }
-  },
-  //   async created() {
-  //     try {
-  //       const response = await fetch("https://zenquotes.io/api/random");
-  //       const data = await response.json();
+    getDaysLeft(dueDate) {
+      const date1 = new Date(dueDate);
+      const date2 = new Date();
+      const diffTime = date1 - date2;
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      return diffDays;
+    },
 
-  //       if (Array.isArray(data) && data.length > 0) {
-  //         this.state.quote = data[0].q; // Extract the quote from the response
-  //         this.state.author = data[0].a;
-  //       } else {
-  //         this.state.quote = "Failed to fetch quote.";
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //       this.state.quote = "Error occurred while fetching quote.";
-  //     }
-  //   },
+
+    //   async created() {
+    //     try {
+    //       const response = await fetch("https://zenquotes.io/api/random");
+    //       const data = await response.json();
+
+    //       if (Array.isArray(data) && data.length > 0) {
+    //         this.state.quote = data[0].q; // Extract the quote from the response
+    //         this.state.author = data[0].a;
+    //       } else {
+    //         this.state.quote = "Failed to fetch quote.";
+    //       }
+    //     } catch (error) {
+    //       console.error(error);
+    //       this.state.quote = "Error occurred while fetching quote.";
+    //     }
+    //   },
+  }
 };
 </script>
 
 <style scoped>
-/* Adjusting date picker size */
-.v-picker--date {
-  max-width: 150px; /* Set the maximum width of the date picker */
+
+</style>
+
+<style>
+.dp__input {
+  min-height: 56px !important;
+  border: 1px solid #9E9E9E !important;
 }
 
-.v-picker__date-table {
-  font-size: 14px; /* Set the font size of the date picker */
+.dp__input:focus {
+  border: 2px solid #000000 !important;
+}
+
+.dp__input:hover {
+  border: 1px solid #000000 !important;
+}
+
+.customize-table {
+  --easy-table-border: 0px;
+  --easy-table-row-border: 0px;
+  --easy-table-body-row-height: 60px;
+}
+
+.vue3-easy-data-table__header {
+  height: 0 !important;
 }
 </style>
